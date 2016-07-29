a2f247c53b7c172f8281ca432162e00600185642
Chase <chase@csh.rit.edu>
2016-07-29 17:26:48 -0400

    Added base code for websockets

--- src/main/kotlin/edu/csh/chase/messaging/MessagingApp.kt
+++ src/main/kotlin/edu/csh/chase/messaging/MessagingApp.kt
@@ -1,11 +1,21 @@
 
 package edu.csh.chase.messaging
 
+import be.tomcools.dropwizard.websocket.WebsocketBundle
 import edu.csh.chase.messaging.config.MessagingConfig
 import io.dropwizard.Application
+import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
 
 class MessagingApp: Application<MessagingConfig>() {
+
+    val websocket = WebsocketBundle()
+
+    override fun initialize(bootstrap: Bootstrap<MessagingConfig>) {
+        super.initialize(bootstrap)
+
+        bootstrap.addBundle(websocket)
+    }
 
     override fun run(config: MessagingConfig, env: Environment) {
 
